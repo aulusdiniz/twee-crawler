7e436a29202da948752c3f1aa43596d1866b3610
Aulus Diniz <aulus999@gmail.com>
2019-03-25 11:56:11 -0300

    basic followers ids code

---
+++ db.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+class MongoAccess(object):
+
+    host = 'localhost'
+    port = 27017
+    database = 'test-database'
+    collection = 'jairbolsonaro_followers'
+    client = MongoClient(host, port)
+
+    def insert_one(self, data):
+        return self.client[self.database][self.collection].insert_one(data).inserted_id
